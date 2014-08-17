dd06729113d91e45ee4aea8b8f19daa273e9c975
Zear <zeartul@gmail.com>
2014-08-17 22:33:38 +0200

    New class for the support of game Objects

---
+++ Object.java
@@ -0,0 +1,47 @@
+public class Object
+{
+	private String name;
+	private x;
+	private y;
+	boolean direction;
+
+	public String getName()
+	{
+		return this.name;
+	}
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	public int getX()
+	{
+		return this.x;
+	}
+
+	public void setX(int x)
+	{
+		this.x = x;
+	}
+
+	public int getY()
+	{
+		return this.y;
+	}
+
+	public void setY(int y)
+	{
+		this.y = y;
+	}
+
+	public boolean getDirection()
+	{
+		return this.direction;
+	}
+
+	public void setDirection(boolean direction)
+	{
+		this.x = x;
+	}
+}
